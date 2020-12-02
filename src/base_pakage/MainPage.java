6323249ca3c594212bf98567ea2bfe2753e382e2
Nikita Makarchuk <vfrfrfcbys@yandex.by>
2020-12-02 19:30:33 +0300

    Added MainPage.java realization

--- src/base_pakage/MainPage.java
+++ src/base_pakage/MainPage.java
@@ -2,6 +2,7 @@ package base_pakage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class MainPage {
     private Filter filter;
@@ -15,13 +16,69 @@ public class MainPage {
     public List<Book> getBooks(BookServices bookServices) {
         //объект в spring должен был создоваться
         Database database = new Database();
+
         List<String> filterList = new ArrayList<>();
+        if (filter.getAlphabetSort()) {
+            filterList.add("alphabetSort");
+        }
 
         return bookServices.getBooks(database, filterList);
     }
 
     public void selectBook(Book book, BookPage bookPage) {
-        //зарускается другая страница
+        //запускается другая страница
+
+        //через spring
+        BookServices bookServices = new BookServices();
+
+        if (user.getAuthorized()) {
+            bookPage.setUserIsAuthorized(true);
+        }
+        bookPage.setIdBook(book.getId());
+
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+        System.out.println("Author: " + book.getAuthor());
+        System.out.println("Rating: " + book.getRating());
+        System.out.println("Description: " + book.getDescription());
+        System.out.println("NameBook: " + book.getName());
+        System.out.println("Comments: " + book.getComments().toString());
+
+        System.out.println("1. download book");
+        System.out.println("2. rate book");
+        System.out.println("3. leave comment");
+        System.out.println("4. exit");
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1: {
+                bookPage.downloadBook(bookServices);
+                return;
+            }
+            case 2: {
+                System.out.println("rate : ");
+                int rate = scanner.nextInt();
+
+                bookPage.rateBook(bookServices, rate);
+                return;
+            }
+            case 3: {
+                System.out.println("comment : ");
+                String comment = scanner.next();
+
+                bookPage.leaveComment(bookServices, comment);
+                return;
+            }
+            case 4: {
+                return;
+            }
+            default: {
+                return;
+            }
+        }
     }
 
     public Boolean authorize(UserService userService) {
@@ -32,15 +89,15 @@ public class MainPage {
 
         User auth_user = userService.authorizate(userName, password, database);
 
-        if( auth_user.getAuthorized()== false){
+        if (auth_user.getAuthorized() == false) {
             return false;
-        }else {
+        } else {
             user = auth_user;
             return true;
         }
     }
 
-    public Boolean registrate(UserService userService){
+    public Boolean registrate(UserService userService) {
         //объект в spring должен был создоваться
         Database database = new Database();
         String userName = new String();
